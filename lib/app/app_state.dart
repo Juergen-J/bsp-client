f3d476c7845651f2e85df691ced83a5eb1dc1839
alexFischer <fischer.alexander.erst@gmail.com>
2025-07-02 19:13:14 +0200

    version 3 for chat UI

--- lib/app/app_state.dart
+++ lib/app/app_state.dart
@@ -43,7 +43,7 @@ class AppState extends ChangeNotifier {
     primaryContainer: Color(0xFFE3E7FB),
     onPrimaryContainer: Color(0xFF2754E8),
     surfaceVariant: Color(0xFFF2F4F7),
-    onSurfaceVariant: Color(0xFF707071),
+    onSurfaceVariant: Color(0xFFD9D9D9),
     outline: Color(0xFFCCCCCC),
   );
 
